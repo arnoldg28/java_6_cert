0dda108f5297210db71069a6c86f57d76724e78c
arnold <arnoldgatmaitan28@gmail.com>
2015-11-01 19:56:31 +0800

    latest

---
+++ Chap_3_Assignments/1_literals_assignments_variables/DoubleToLong.java
@@ -0,0 +1,6 @@
+class DoubleToLong{
+	public static final void main(String[] args){
+		double d = 100L; //This is ok.
+		long l = 100d; //Is this OK?
+	}
+}
---
+++ Chap_3_Assignments/1_literals_assignments_variables/FloatToShort.java
@@ -0,0 +1,7 @@
+class FloatToShort{
+	public static final void main(String args[]){
+		float f = 234.56F;
+		short s = (short)f;
+		System.out.println(s);
+	}
+}
